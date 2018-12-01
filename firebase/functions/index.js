b595079ac66d33aa13627fc10294f068c8c15f4e
Martin Dluhoš <martin.dluhos@incentage.com>
2018-12-02 00:35:03 +0100

    Improved parsing and added handler for no options

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -11,6 +11,9 @@ function Recipe(name, ingredients) {
     this.ingredients = ingredients;
 }
 
+// How many recipes to select?
+const maxSelectedNum = 3;
+
 app.intent('WelcomeAndLearnIngredients', (conv) => {
     conv.ask('Welcome to Fridge Manager. Which ingredients do you have available? For example, you can say onion, potatoes, and pineapple.');
 })
@@ -18,14 +21,16 @@ app.intent('WelcomeAndLearnIngredients', (conv) => {
 app.intent('RecipeMatching', (conv, {ingredients}) => {
     console.log(ingredients);
     
+    // Test recipes
     let recipe1 = new Recipe('spaghetti with ketchup and cheese', ['spaghetti','ketchup', 'cheese', 'garlic', 'onion', 'salt', 'oil']);
     let recipe2 = new Recipe('schnitzel with potatoes', ['potatoes', 'pork', 'oil', 'flour', 'breadcrumbs', 'eggs', 'salt', 'butter']);
     let recipe3 = new Recipe('risotto', ['rice', 'corn', 'carrot', 'onion', 'cheese', 'olives', 'mushrooms', 'salt', 'oil']);
-    
-    let recipes = [ recipe1, recipe2, recipe3 ];
+    let recipe4 = new Recipe('pancakes', ['milk', 'flour', 'sugar', 'eggs', 'baking powder', 'salt', 'oil']);
+    let recipes = [ recipe1, recipe2, recipe3, recipe4 ];
     
     let matchedRecipes = [];
     
+    // Determine matched recipes
     recipes.forEach(function(recipe) { 
         
         let matchedIngredients = [];
@@ -38,12 +43,58 @@ app.intent('RecipeMatching', (conv, {ingredients}) => {
                 matchedRecipes.push(recipe);
             }
         }
-
     });
 
-    console.log(matchedRecipes);
-    conv.contexts.set('recipe-options', 10, {"1": recipe1, "2": recipe2, "3": recipe3});
-    conv.ask('These are your options. ' + recipe1.name + ', ' + recipe2.name + ', or ' + recipe3.name + '? Would you like number 1, 2, or 3?');
+    // Select the highest ranked recipes
+    let selectedRecipes = matchedRecipes.slice(0, maxSelectedNum);
+    let selectedNum = selectedRecipes.length;
+    let lastIndex = selectedNum - 1;
+    
+    let recipeOptions = {};
+    let optionsQuery = 'These are your options: ';
+    selectedRecipes.forEach(function(recipe, index, selectedRecipes) {
+        // Save the retrieved recipes to context
+        recipeOptions[index+1] = recipe;
+        
+        // List the options to the user
+        optionsQuery += recipe.name;
+        
+        if (index == lastIndex) {
+            optionsQuery += '. ';
+        }
+        else if (index == lastIndex-1)  {
+            optionsQuery += ', and ';
+        }
+        else {
+            optionsQuery += ', ';
+        }
+    })
+    optionsQuery += 'Which one would you like choose? Option '
+    
+    for (let i = 0; i < selectedNum; i++) {
+         
+        // We start counting from 0, but most users start with 1:)
+        optionsQuery += i+1;
+
+        if (i == lastIndex) {
+            optionsQuery += '?';
+        }
+        else if (i == lastIndex-1)  {
+            optionsQuery += ', or ';
+        }
+        else {
+            optionsQuery += ', ';
+        }
+    }
+
+    if (selectedRecipes.length !== 0) {
+        conv.contexts.set('recipe-options', 10, recipeOptions);
+        conv.ask(optionsQuery);
+    }
+    else {
+        // We don't have any possible recipes to offer 
+        conv.close('Unfortunately, we have not found any matching recipe for your ingredients. You might consider paying a visit to a grocery store.');
+    }
 })
 
 app.intent('PresentRecipe', (conv, {number}) => {
